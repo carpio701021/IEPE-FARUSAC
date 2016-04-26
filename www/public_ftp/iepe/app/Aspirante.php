2dbb241becbd5aab82780a115990ab943778c015
jose.chavarria <jodaches@gmail.com>
2016-04-26 12:44:29 -0600

    verificación de formulario corregida y redirección correcta hacia perfil o formulario

--- www/public_ftp/iepe/app/Aspirante.php
+++ www/public_ftp/iepe/app/Aspirante.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Aspirante extends Model
 {	
     public function Formulario(){
-    	return $this->hasMany('App\Formulario','NOV');
+    	return $this->hasMany('App\Formulario','NOV','NOV');
     }
 }
