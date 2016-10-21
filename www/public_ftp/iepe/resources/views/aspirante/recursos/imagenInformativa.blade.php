bbda07a2dc86da1d633e7da19518ae71c6a85bdc
Javier Carpio <carpio701021@gmail.com>
2016-10-20 20:49:21 -0600

    Quitando todo para recargar imagenes

--- www/public_ftp/iepe/resources/views/aspirante/recursos/imagenInformativa.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.aspirante-layout')
-
-@section('content')
-    <div class="row">
-        <div class="col-lg-12">
-            <img width="100%" src="{{ url('/aspirante_public/img/'.json_decode(file_get_contents(storage_path().'/recursos.json'),TRUE)['imagen_informativa']) }}" />
-        </div>
-    </div>
-@endsection
--- www/public_ftp/iepe/resources/views/auth/emails/password.blade.php
+++
@@ -1,1 +0,0 @@
-Sigue el siguiente enlace para restablecer tu contraseña: <a href="{{ $link = url('/aspirante/password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
